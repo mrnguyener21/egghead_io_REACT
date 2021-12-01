d88dfe2a2b0e7c97f3d0ea6603fc4c7441788f69
mrnguyener21 <victortnguyen.workemail@gmail.com>
2021-12-01 07:29:29 -0800

    reviewed react fragments

--- eggHead.js
+++ eggHead.js
@@ -1,1 +1,7 @@
-// test
+//REACT FRAGMENT
+//react fragment lets us use multiple JSX elements when calling it with reactDOMrender()
+//react fragment can be noted as <> </> aka blank tags
+
+<>
+{/* content */}
+</>
